3579334152fc866f7a29be35ffa4588f0a353d4a
ΔΗΜΗΤΡΗΣ ΚΥΡΙΤΣΗΣ <dim.kuritshs@gmail.com>
2024-08-07 14:21:59 +0300

    Επίλυση συγκρούσεων

--- server.js
+++ server.js
@@ -1,25 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');  // Προσθήκη του cors
+const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3001;
 
-let temperatureData = [];
+// Σύνδεση με τη βάση δεδομένων MongoDB Atlas
+mongoose.connect('mongodb+srv://dimkuritshs:Jimkiritsis1#@cluster0.ihfid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Ορισμός σχήματος για τα δεδομένα θερμοκρασίας
+const temperatureSchema = new mongoose.Schema({
+    temperature: Number,
+    timestamp: String
+});
+
+const Temperature = mongoose.model('Temperature', temperatureSchema);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());  // Χρήση του cors
+app.use(cors());
 
 app.post('/data', (req, res) => {
     const { temperature } = req.body;
     const timestamp = new Date().toISOString();
-    temperatureData.push({ temperature, timestamp });
-    console.log(`Received data: ${temperature} °C at ${timestamp}`);
-    res.sendStatus(200);
+
+    const newTemperature = new Temperature({
+        temperature,
+        timestamp
+    });
+
+    newTemperature.save(err => {
+        if (err) {
+            res.sendStatus(500);
+            console.error(err);
+        } else {
+            console.log(`Received data: ${temperature} °C at ${timestamp}`);
+            res.sendStatus(200);
+        }
+    });
 });
 
 app.get('/getTemperatures', (req, res) => {
-    res.json(temperatureData);
+    Temperature.find({}, (err, temperatures) => {
+        if (err) {
+            res.sendStatus(500);
+            console.error(err);
+        } else {
+            res.json(temperatures);
+        }
+    });
 });
 
 app.listen(port, () => {
