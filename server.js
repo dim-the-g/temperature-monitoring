a9e099bdd398693bce0ce042e55f47cd91d07b45
ΔΗΜΗΤΡΗΣ ΚΥΡΙΤΣΗΣ <dim.kuritshs@gmail.com>
2024-08-07 15:52:33 +0300

    Επίλυση συγκρούσεων

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 app.post('/data', async (req, res) => {
     const { temperature } = req.body;
-
-    // Ελέγξτε αν η θερμοκρασία είναι αριθμός
+    console.log('Received data:', req.body);
+  
     const numericTemperature = parseFloat(temperature);
-
     if (isNaN(numericTemperature)) {
-        console.error('Invalid temperature value:', temperature);
-        return res.status(400).send('Invalid temperature value');
+      console.error('Invalid temperature value:', temperature);
+      return res.status(400).send('Invalid temperature value');
     }
-
+  
     const timestamp = new Date().toISOString();
-
     const newTemperature = new Temperature({
-        temperature: numericTemperature,
-        timestamp
+      temperature: numericTemperature,
+      timestamp
     });
-
+  
     try {
-        await newTemperature.save();
-        console.log(`Received data: ${numericTemperature} °C at ${timestamp}`);
-        res.sendStatus(200);
+      await newTemperature.save();
+      console.log(`Data saved: ${numericTemperature} °C at ${timestamp}`);
+      res.sendStatus(200);
     } catch (err) {
-        res.sendStatus(500);
-        console.error(err);
+      console.error('Error saving data:', err);
+      res.sendStatus(500);
     }
-});
+  });
+  
