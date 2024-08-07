7d6f689b83b294a78c98b01bc38a24d723e3b906
ΔΗΜΗΤΡΗΣ ΚΥΡΙΤΣΗΣ <dim.kuritshs@gmail.com>
2024-08-07 15:56:20 +0300

    Επίλυση συγκρούσεων

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3001;
+
+// Σύνδεση με τη βάση δεδομένων MongoDB Atlas
+mongoose.connect('mongodb+srv://dimkuritshs:jimkiritsis123@cluster0.ihfid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Ορισμός σχήματος για τα δεδομένα θερμοκρασίας
+const temperatureSchema = new mongoose.Schema({
+    temperature: Number,
+    timestamp: String
+});
+
+const Temperature = mongoose.model('Temperature', temperatureSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
 app.post('/data', async (req, res) => {
     const { temperature } = req.body;
     console.log('Received data:', req.body);
@@ -24,3 +49,17 @@ app.post('/data', async (req, res) => {
     }
   });
   
+
+app.get('/getTemperatures', async (req, res) => {
+    try {
+        const temperatures = await Temperature.find({});
+        res.json(temperatures);
+    } catch (err) {
+        res.sendStatus(500);
+        console.error(err);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
